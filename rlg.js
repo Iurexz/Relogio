16dfafe8d0fd07e029ac690958583b4a79c8be35
Iure Costa <131163902+Iurexz@users.noreply.github.com>
2023-09-25 10:52:27 -0300

    rlg.js

--- rlg.js
+++ rlg.js
@@ -15,9 +15,19 @@ function relogio (){
     let hoje = new Date()
     let hr =  hoje.getHours()
     let min = hoje.getMinutes()
-    let sec = hoje.getSeconds()
+    let seg = hoje.getSeconds()
+
+if(hr<10){
+    hr="0"+ hr
+}
+if(min<10){
+    min="0"+ min
+}
+if(seg<10){
+    seg="0"+ seg
+}
 
     horas.textContent = hr
     minutos.textContent = min
-    segundos.textContent = sec
+    segundos.textContent = seg
 }
